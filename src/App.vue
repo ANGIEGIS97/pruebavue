f646c9e33a528d40104471a62a2fff0e3f1d3b2b
ANGIEGIS97 <angiedeconejito@gmail.com>
2023-06-23 16:40:53 -0500

    imagen perrito

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,13 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div class="text-center selection:bg-green-100">
-    <img class="mx-auto mt-12 mb-8" alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Vite + Vue 3 + Tailwindcss v3" />
+  <div class="rounded-lg text-center selection:bg-green-100">
     <img
-      class="mx-auto my-8"
-      alt="powered-by"
-      src="./assets/powered-by-vitawind-bright.png"
+      class="mx-auto mb-8 mt-12 rounded-lg"
+      alt="Vue logo"
+      src="https://picsum.photos/id/237/200/300"
     />
+    <HelloWorld msg="Hola Mundo" />
   </div>
 </template>
 
